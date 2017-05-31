ad51fee7bdda60eff43c95ed558bdf20377322bc
daheinze <daheinze@JARVIS_WORK>
2017-05-31 12:55:59 +0200

    Added English

--- QnABot/QnABot/Dialogs/RootDialog.cs
+++ QnABot/QnABot/Dialogs/RootDialog.cs
@@ -21,24 +21,8 @@ namespace QnABot.Dialogs
             var activity = await result as Activity;
 
             // Prompt text
-            await context.PostAsync("Was möchtest du wissen?");
+            await context.PostAsync("What do you want to do?");
             context.Wait(QnADialog);
-        }
-
-        private async Task MessageContinueAsync(IDialogContext context, IAwaitable<object> result)
-        {
-            var activity = await result as Activity;
-
-            // Prompt text
-            await context.PostAsync("Kann ich dir noch weiterhelfen?");
-            context.Wait(QnADialog);
-        }
-
-        private async Task DialogEndAsync(IDialogContext context, IAwaitable<object> result)
-        {
-            await context.PostAsync("Ich hoffe, ich konnte dir helfen. Einen schönen Tag noch!");
-
-            await this.StartAsync(context);
         }
 
         private async Task QnADialog(IDialogContext context, IAwaitable<object> result)
@@ -46,16 +30,11 @@ namespace QnABot.Dialogs
             var activityResult = await result as Activity;
             var query = activityResult.Text;
 
-            if (query.ToLower() == "nein")
-            {
-                context.Wait(DialogEndAsync);
-            }
-
             var qnaResult = QnaApi.GetFirstQnaAnswer(query);
 
             if (qnaResult == null)
             {
-                await context.PostAsync("Das kann ich leider nicht beantworten.");
+                await context.PostAsync("I can't answer that.");
             }
             else
             {
@@ -63,7 +42,7 @@ namespace QnABot.Dialogs
             }
                 
 
-            context.Wait(MessageContinueAsync);
+            context.Wait(MessageReceivedAsync);
         }
 
     }
